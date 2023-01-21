68bee26fdda07081872bca3359df947efc23e92f
nikhil296 <nikhilgautam1729@gmail.com>
2023-01-21 23:47:39 +0530

    DP ++

---
+++ Dynamic_Programming/Babbar/Reach_a_given_score/dp_tabulation.java
@@ -0,0 +1,20 @@
+// Time : O(n), Space : O(n).
+package Dynamic_Programming.Babbar.Reach_a_given_score;
+import java.util.*;
+public class dp_tabulation {
+    public long count(int n) {
+        long[] dp = new long[(int)n+1];
+        Arrays.fill(dp, 0);
+        dp[0] = 1;
+        for(int i = 3; i<=n; i++) {
+            dp[i] += dp[i-3];
+        }
+        for(int i = 5; i<=n; i++) {
+            dp[i] += dp[i-5];
+        }
+        for(int i = 10; i<=n; i++) {
+            dp[i] += dp[i-10];
+        }
+        return dp[n];
+    }
+}
