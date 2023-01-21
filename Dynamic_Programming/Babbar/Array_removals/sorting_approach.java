04db50359338a89258c744be21384790b16cda5e
nikhil296 <nikhilgautam1729@gmail.com>
2023-01-21 23:03:09 +0530

    DP ++

---
+++ Dynamic_Programming/Babbar/Array_removals/sorting_approach.java
@@ -0,0 +1,25 @@
+// Time : O(n * log n), Space : O(n).
+package Dynamic_Programming.Babbar.Array_removals;
+import java.util.*;
+public class sorting_approach {
+    int removals(int[] arr, int n, int k) {
+        if(n == 1) return 0;
+        Arrays.sort(arr);
+        int ans = 0;
+        // below loop. runs for -> O(n) time only, itws better that dp_tabulation approach.
+        for(int i = 0, j = 0; i<n && j<n;) {
+            if(i == j) {
+                j++;
+            }
+            if(j<n && arr[j] - arr[i] <= k) {
+                if(j-i > ans) {
+                    ans = j-i;
+                }
+                j++;
+            } else {
+                i++;
+            }
+        }
+        return (n - (ans + 1));
+    }
+}
